ea04da7b6d708c2384944e89c41fb4a1a7c9490c
csengeattila <csenge.attila@gmail.com>
2021-01-16 15:06:42 +0100

    Update postInstall.sh

--- postInstall.sh
+++ postInstall.sh
@@ -3,4 +3,9 @@
 cd /opt && sudo git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R $USER:$USER /opt/yay-git
 cd /opt/yay-git && makepkg -si --noconfirm
+
+sudo pacman -S sudo pacman -S xorg
+sudo pacman -S sudo pacman -S gnome-shell gdm dolphin konsole gnome-control-center ntfs-3g gwenview --noconfirm
+sudo systemctl enable gdm
+
 reboot
