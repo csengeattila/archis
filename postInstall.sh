158f8e6e3470a15cc1fce49284417edc777cfacb
csengeattila <csenge.attila@gmail.com>
2021-01-16 15:29:49 +0100

    Add files via upload

--- postInstall.sh
+++ postInstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+# BSPWM ----------------------------------------------------------------------
+sudo pacman -S  xorg xorg-xinit bspwm sxhkd dmenu nitrogen feh picom arandr --noconfirm
+
+# Programok -------------------------------------------------------------------
+
 cd /opt && sudo git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R $USER:$USER /opt/yay-git
 cd /opt/yay-git && makepkg -si --noconfirm
-
-sudo pacman -S sudo pacman -S xorg
-sudo pacman -S sudo pacman -S gnome-shell gdm dolphin konsole gnome-control-center ntfs-3g gwenview --noconfirm
-sudo systemctl enable gdm
-
 reboot
